6014bec9318390d92b3943ecacf9e2947f449ed0
Stephen Wright <scw248@gmail.com>
2019-01-03 22:10:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,7 @@
 
 def prime?(num)
   return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  range = 2..num
+  range.each {|i| return false if num % i == 0}
   true
 end
